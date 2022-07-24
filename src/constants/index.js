dd87111d744278fac426ecc287f0b7557472e05b
Vadim <60232088+vadim-mozgovoy@users.noreply.github.com>
2022-07-25 00:06:32 +0300

    Revert "Updates"
    This reverts commit fe7e74fec93e30e8ea9336f13d85186d48875822.

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const ANAKIN_IMAGE = "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg";
+export const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+export const PostPerson = [
+    {
+        name: "Anakin Skywalker",
+        photo: ANAKIN_IMAGE,
+        nickname: "@dart_vader",
+        content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+        image: RAY_IMAGE,
+        date: "26 July",
+        comments: "482",
+        reposts: "182",
+        likes: "887",
+    },
+];
+
+
